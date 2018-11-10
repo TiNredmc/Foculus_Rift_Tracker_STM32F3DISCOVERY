9025eaf8b5955da7e4857ba34815d13c9d6c75bb
TINLΞTHΛX <thipok17@hotmail.com>
2018-11-10 21:25:21 +0700

    this is flash script (change the dir to your own )

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 # stop remaining openocd instances
 killall -9 openocd
 # start new openocd and reset and init chip
-openocd -f /usr/local/share/openocd/scripts/board/stm32f3discovery.cfg -c init -c"reset init" -c"poll"
+openocd -f /usr/share/openocd/scripts/board/stm32f3discovery.cfg -c init -c"reset halt" -c"flash erase_sector 0 0 127" -c"flash write_image stm32f3_HID_for_real.elf"
